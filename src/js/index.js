f198b59af4362e2a4f281f92a6125566106a5349
Denis Bikbaev <arxail7@gmail.com>
2023-03-26 18:00:04 +0300

    add desktop

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  grabCursor: true,
+  keyboard:{
+    enable: true,
+    onlyInViewport: true,
+    pageUpDown:true,
+  },
+});
